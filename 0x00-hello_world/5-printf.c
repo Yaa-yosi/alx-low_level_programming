1d0f4aec4dc456e555e949bdf4cc524cdc54c88c
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-07 17:02:28 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the beginning point of the program
+ *
+ * Return: this is where the execution process ends
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
