a14f837db236cf54367c19136326d785aa0248ba
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-13 14:52:03 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,15 @@
 #include "main.h"
+
 /**
- *times_table -  prints the 9 times table
- *Return: void
+ * times_table - prints the 9 times table
+ * Return: void
  */
 void times_table(void)
 {
-	int a, b, p;/*p for product*/
+    int a, b, p;
 
-	for (a = 0; a <= 9; a++)
-	{
-		for (b = 0; b <= 9; b++)
-		{
-			p = a * b;
-			if (p > 9)
-			{
-			_putchar(p / 10 + '0');
-			_putchar(p % 10 + '0');
-			}
-			else
-				_putchar(p + '0');
-			if  (b != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
-}
+    for (a = 0; a <= 9; a++)
+    {
+        for (b = 0; b <= 9; b++)
+        {
+            p = a * b;
