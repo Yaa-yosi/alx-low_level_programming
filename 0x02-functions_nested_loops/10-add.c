9120bea6a53772fa5ca344656fddb7c097b48582
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-13 06:47:00 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *add - adds sum
+ *@int: represents values
+ *Return: sum
+ */
+int add(int a , int b)
+{
+
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *jack_bauer - print out minutes
+ *Reurn: returns 0
+ */
+void jack_bauer(void)
+{
+	int h; /*h for hour*/
+	int m; /*m for minutes*/
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0);
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
