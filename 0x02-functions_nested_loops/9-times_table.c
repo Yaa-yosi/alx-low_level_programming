9b5a95500d28becb5cb49f12425e8d8cef41a1d9
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-14 03:24:09 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,33 @@
  */
 void times_table(void)
 {
-    int a, b, p;
+	int a, b, c, d, e;
 
-    for (a = 0; a <= 9; a++)
-    {
-        for (b = 0; b <= 9; b++)
-        {
-            p = a * b;
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (c > 9)
+			{
+				d = c % 10;
+				e = (c - d) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(e + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				if (b != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
