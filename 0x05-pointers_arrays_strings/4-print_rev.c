0e2ac3cf155ea2e6007a077df61067367ee312a7
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-18 20:01:04 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_rev - print in reverse
+ *@s: value
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
