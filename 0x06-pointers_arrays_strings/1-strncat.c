41f9cd3523cf1907d8ca0426fcd2737739bf0056
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-21 16:08:53 -0700

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ *_strncat - concatenate two strings using strncat
+ *@dest: destination string
+ *@src: source string
+ *@n: string length to concatenate
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
