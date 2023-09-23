d58fe3a12f6343c1d8da1a62a0b8a2120d8ef7a6
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-22 18:31:23 -0700

    4-rev_array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
- *
+ *reverse_array - reverse array
+ *@a: string
+ *@n: index
+ *Return: void
  */
 void reverse_array(int *a, int n)
 {
@@ -19,6 +17,6 @@ void reverse_array(int *a, int n)
 	printf("\n");
 	for (i = n - 1; i >= 0; i--)
 	{
-		printf("%d ,", a[i]);
+		printf("%d, ", a[i]);
 	}
 }
