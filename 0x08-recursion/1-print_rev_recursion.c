b7941707c81887bd915f28bb1d9cff4a880a93c1
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-27 16:47:16 -0700

    1-print_rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - print in reverse
+ *@s: string to be reversed
+ *Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
