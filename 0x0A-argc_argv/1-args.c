5a2c38d18b829f5911a2d2b61ebaa593ecb5df67
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-09-29 18:59:39 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print the number of arguement
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
