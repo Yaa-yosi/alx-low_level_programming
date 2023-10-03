c441cf1eaead6c56267a664b49d687c8dc0e0d0f
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-03 16:36:23 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	while (str[len] != '\0')
 		len++;
 	ptr = malloc((len * sizeof(char)) + 1);
-	if (str == NULL)
+	if (ptr == NULL)
 		return (NULL);
 	strcpy(ptr, str);
 	return (ptr);
