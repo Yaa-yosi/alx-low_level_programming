b03e78557a41c39824cd186ed823e2710abef84f
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-03 07:29:26 -0700

    creating an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	 ptr =(char *)malloc(size * sizeof(char));
+	 ptr = (char *)malloc(size * sizeof(char));
 
 	if (ptr == NULL)
 		return (NULL);
