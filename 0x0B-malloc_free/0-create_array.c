18ecaacc069e1e1cfb44a32def359d9c6d2b7f45
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-03 16:28:24 -0700

    task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
-
 	 ptr = (char *)malloc(size * sizeof(char));
-
-	if (ptr == NULL)
+	 if (size == 0 && ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_strdup - return pointer to a newly allocated memory
+ *@str: string
+ *Return: null or string
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	ptr = malloc((len * sizeof(char)) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	strcpy(ptr, str);
+	return (ptr);
+}
+
