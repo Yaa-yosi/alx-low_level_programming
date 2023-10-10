4633fdfc808a26bbb99d6ff40f3b46d44cc89433
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-09 17:03:12 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - creating a stuct dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of doc
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
