4450626a5a58d4c9d64610c022de95077185bfae
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-09 19:03:51 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,8 @@ void print_dog(struct dog *d);
  * dog_t - new name fot struct dog
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
