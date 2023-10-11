f6e1c9ac9cf58caae3952eb4f7b499510efcb8a0
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-11 15:46:53 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ *print_name - prints a name
+ *@name: contains the string
+ *@f: pointer to the function
+ *Return: void
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL || name != NULL)
+	f(name);
+}
