ada46a972fed457bc16d6447f79a0878f3c1ffd7
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-24 17:28:15 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 
 	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	else
 		return (NULL);
 	tmp = *head;
 
