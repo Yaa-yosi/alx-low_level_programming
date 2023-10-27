0f56124d9487c2e062cdecb1a190d25f84620a0a
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-26 19:38:31 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,7 +6,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, p2 = 1;
+	int i;
 	unsigned int res = 0;
 
 	if (b == NULL)
@@ -16,11 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		if (b[i] == '1')
-		{
-			res = res + p2;
-		}
-		p2 = p2 * 2;
+		res = 2 * res + (b[i] - '0');
 	}
 	return (res);
 }
