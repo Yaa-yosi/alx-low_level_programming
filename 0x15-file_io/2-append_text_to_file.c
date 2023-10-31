9b6b00573d6b1231c9218e71251ae51892d3a28b
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-10-30 18:47:05 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		close(fd);
 	str_len = strlen(text_content);
 
 	br = write(fd, text_content, str_len);
