8260411b2bd2027c4890fe17731bc92baeac777b
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-13 10:55:43 +0000

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,23 +7,17 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *h;
 	dlistint_t *tmp;
 
 	tmp = malloc(sizeof(dlistint_t));
 	if (tmp == NULL)
 		return (NULL);
-	tmp->n = n;
 	tmp->prev = NULL;
-	h = *head;
-	if (h != NULL)
-	{
-		while (h->prev != NULL)
-			h = h->prev;
-	}
-	tmp->next = h;
-	if (h != NULL)
-		h->prev = tmp;
+	tmp->n = n;
+	tmp->next = NULL;
+	tmp->next = *head;
+	if (*head != NULL)
+		(*head)->prev = tmp;
 	*head = tmp;
-	return (tmp);
+	return (*head);
 }
