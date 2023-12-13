f4321d9de4221119641c911c7c295d8bc6644923
Mercy Agbasah <agbasahmercy761@gmail.com>
2023-12-13 01:35:29 +0000

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,7 +7,10 @@ void free_dlistint(dlistint_t *head)
 {
 	if (head == NULL)
 		return;
-	while (head->next)
+	while (head->next != NULL)
+	{
 		head = head->next;
+		free(head->prev);
+	}
 	free(head);
 }
